867eec97175c5af7d7c1303a011afa948a56bab0
HudsonOnHere <wright.richard94@icloud.com>
2021-06-08 21:31:20 -0400

    code cleanup, main functionality is still the same as legacy versions. needs some further testing

--- di_tool_0.5.sh
+++ di_tool_0.5.sh
@@ -1,137 +1,276 @@
 #!/bin/bash
 
+
 clear
 echo
-echo "di_tool 0.4.2 -- by Rich Wright"
+echo "di_tool 0.5 -- by Rich Wright"
 echo
 echo "run me as root or you will be prompted to enter your password"
 echo
 
-# stage1 assigns the paths to the source for data transfer
-function stage1 {
-# source volume assigned
-echo
-echo "Where is your data located?"
-echo
-cd /Volumes/
-PS3='Enter your source volume: '
-select source_vol in *;
-	do
-		case $source_vol in
-			*)		
-				clear;
-				echo
-				echo "You picked " $source_vol;
-				echo;
-				break
-				;;
-		esac
-	done
 
-# source home folder assigned
-echo
-echo "Which user's data would you like to transfer?"
-echo
-cd "/Volumes/$source_vol/Users/"
-PS3='Enter your source home folder: '
-select source_user in *;
-	do
-		case $source_user in
-			*)		
-				clear;
-				echo;
-				echo "You picked " $source_user;
-				echo;
-				break
-				;;
-		esac
-	done
-# final path to the source home folder
-src=$(echo -n "/Volumes/$source_vol/Users/$source_user")
 
-}
 
-# stage2 assigns the paths to the destination for data transfer
-function stage2 {
-# target volume assigned
-echo
-echo "Where is your data going?"
-echo
-cd /Volumes/
-PS3='Enter your destination volume: '
-select target_vol in *;
-	do
-		case $target_vol in
-			*)
-				clear;
-				echo
-				echo "You picked " $target_vol
-				echo;
-				break
-				;;
-		esac
-	done
+# user selects their source volume and source home folder
 
-# target home folder assigned
-echo
-echo "Which user is going to recieve this data?"
-echo
-cd "/Volumes/$target_vol/Users/"
-PS3='Enter your destination home folder: '
-select target_user in *;
-	do
-		case $target_user in
-			*)
-				clear;
-				echo
-				echo "You picked " $target_user
-				echo;
-				break
-				;;
-		esac
-	done
-# final path to the destination home folder
-tgt=$(echo -n "/Volumes/$target_vol/Users/$target_user")
+function SOURCE {
+
+	# source volume assigned
+	
+	echo
+	echo "Where is the data currently located?"
+	echo
+	
+	cd /Volumes/
+	
+	PS3='Select a source volume: '
+	
+	select sourceVolume in *;
+	
+		do
+		
+			case $sourceVolume in
+			
+				*)		
+				
+					clear ;
+					
+					echo ;
+					
+					echo "You picked " $sourceVolume ;
+					echo;
+					
+					break ;;
+					
+			esac
+			
+		done
+
+
+
+
+
+	# source home folder assigned
+	echo
+	echo "Which user's data is being transferred?"
+	echo
+	
+	cd "/Volumes/$sourceVolume/Users/"
+	
+	PS3='Select a source folder: '
+	
+	select sourceUser in *;
+	
+		do
+		
+			case $sourceUser in
+			
+				*)		
+				
+					clear ;
+					
+					echo ;
+					
+					echo "You picked " $sourceUser ;
+					
+					echo ;
+					
+					break ;;
+					
+			esac
+			
+		done
+		
+		
+	
+	
+	# final path to the source home folder, assigned to variable
+	
+	src=$(
+		echo -n "/Volumes/$source_vol/Users/$source_user"
+		)
 
 }
 
-stage1
-
-clear
-echo
-echo "You are going to transfer $source_user's data
-
-You can enter Y to continue, N to start over, or Q to quit"
-while true; do
-	read -p "Continue?" -n 1 yn
-		case $yn in
-			[Yy]) stage2; clear; break;;
-			[Nn]) stage1; clear;;
-			[Qq]) exit;;
-		esac
-done
 
 
-clear
-echo
-echo "You are going to transfer $source_user's data to $target_user's home folder, located at $tgt
+# user selects their destination volume and destination folder
 
-You can enter Y to continue, N to start over, or Q to exit"
-while true; do
-	read -p "Ready to transfer?" -n 1 yn
-		case $yn in
-			[Yy]) break;;
-			[Nn]) stage1; clear; stage2; clear;;
-			[Qq]) exit;;
-		esac
-done
+function DESTINATION {
+
+	# target volume assigned
+	echo
+	echo "Where are you transferring this data to?"
+	echo
+
+	cd /Volumes/
+
+	PS3='Select a destination volume: '
+
+	select destVolume in *;
+
+		do
+	
+			case $destVolume in
+		
+				*)
+			
+					clear ;
+				
+					echo ;
+				
+					echo "You picked " $destVolume ;
+				
+					echo ;
+				
+					break ;;
+
+			esac
+
+		done
 
 
-# Bulk data transfer from default home folders
-sudo rsync -avr --progress "$src/Destkop" "$src/Documents" "$src/Downloads" "$src/Movies" "$src/Music" "$src/Pictures" "$tgt/"
 
-# Delete new application dependencies
-sudo rm -rf "$tgt/Library/Safari" "$tgt/Library/Calendars" "$tgt/Library/Mail" "$tgt/Library/Application\ Support/AddressBook"
 
-# Rebuild old application dependencies
-sudo rsync -avr --progress "$src/Library/Safari" "$src/Library/Calendars" "$src/Library/Mail" "$tgt/Library/" && sudo rsync -avrz "$src/Library/Application\ Support/AddressBook" "$tgt/Library/Application\ Support/"
+
+	# target home folder assigned
+	echo
+	echo "Which user is going to recieve this data?"
+	echo
+
+	cd "/Volumes/$target_vol/Users/"
+
+	PS3='Select a destination folder: '
+
+	select destnUser in *;
+
+		do
+	
+			case $desUser in
+		
+				*)
+			
+					clear ;
+				
+					echo ;
+				
+					echo "You picked " $destUser ;
+				
+					echo ;
+				
+					break ;;
+
+			esac
+		
+		done
+	
+	
+	# final path to the destination home folder
+	dest=$(
+		echo -n "/Volumes/$target_vol/Users/$target_user"
+		)
+
+
+}
+
+
+
+
+# contains two(2) prompts that validate the user's input
+# gives the user a chance to start over in case of mistake at either selection stage
+
+function INPUTVALIDATION {
+
+	clear
+	echo
+	echo "You are going to transfer $sourceUser's data"
+	echo
+	echo "You can enter Y to continue, N to start over, or Q to quit"
+
+	while true; do
+
+		read -p "Continue?" -n 1 yn
+	
+			case $yn in
+		
+				[Yy])
+				
+					DESTINATION ;
+				
+					clear;
+				
+					break;;
+			
+				[Nn])
+				
+					SOURCE ;
+				
+					clear ;;
+			
+				[Qq])
+			
+					exit ;;
+				
+			esac
+		
+	done
+
+
+	clear
+	echo
+	echo "You are going to transfer $sourceUser's data to $destUser's home folder, located at $dest"
+	echo
+	echo "You can enter Y to continue, N to start over, or Q to exit"
+
+	while true; do
+
+		read -p "Ready to transfer?" -n 1 yn
+	
+			case $yn in
+		
+				[Yy])
+			
+					break;;
+			
+				[Nn])
+			
+					SOURCE ;
+				
+					clear ;
+				
+					DESTINATION ;
+				
+					clear ;;
+				
+				[Qq])
+			
+					exit ;;
+				
+			esac
+		
+	done
+
+}
+
+
+
+# the core functionality of the program, contains instructions for transferring the data
+# rsync is not scripted here to with the inclusion of 'sudo'
+# it may fail if the script itself is not run with 'sudo'
+
+function DATATRANSFER {
+
+	# Bulk data transfer from default home folders
+	rsync -avr --progress "$src/Destkop" "$src/Documents" "$src/Downloads" "$src/Movies" "$src/Music" "$src/Pictures" "$dest/"
+
+
+	# Delete new application dependencies
+	rm -rf "$dest/Library/Safari" "$dest/Library/Calendars" "$dest/Library/Mail" "$dest/Library/Application\ Support/AddressBook"
+
+
+	# Rebuild old application dependencies
+	rsync -avrz "$src/Library/Application\ Support/AddressBook" "$tgt/Library/Application\ Support/" &
+
+	rsync -avr --progress "$src/Library/Safari" "$src/Library/Calendars" "$src/Library/Mail" "$tgt/Library/"
+
+}
